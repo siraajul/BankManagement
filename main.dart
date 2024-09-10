8a157fb5f3267c1f1da3748bb251d13fb6e253e8
siraajul <shirajulislamparvez@gmail.com>
2024-09-10 21:32:27 +0600

    Deposit Method Added

--- main.dart
+++ main.dart
@@ -2,4 +2,17 @@ class Account {
   String userName = 'Ostad';
   String userAccountNumber = '12345';
   double accountBalance = 10000;
+
+  //Deposit
+  void balanceDeposit(double amount) {
+    if (amount > 0) {
+      accountBalance = accountBalance + amount;
+      print('Successfully Deposited');
+      print('New Balance is $accountBalance');
+    } else {
+      print('Add Valid Number');
+    }
+
+  }
+
 }
